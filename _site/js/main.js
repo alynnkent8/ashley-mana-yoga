f64a4dc7f61ddbf054364f0e210f180968cb9cd5
alynnkent8 <Ashley@Ashleys-MacBook-Air.local>
2017-02-12 10:45:50 +0800

    initial commit

---
+++ _site/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.profile-imgs').owlCarousel({
+      loop: true,
+      margin: 10,
+      nav:true,
+      autoplay: 50,
+      items:1,
+      autoSpeed: 500
+  })
+});
---
+++ js/script.js
@@ -0,0 +1,40 @@
+// Data Types
+// "hello world" - String
+// 1, 234, -34532, -5902 - Integers
+// 1.2, -0.9, 3.345 - Floats
+//true, false - Booleans
+
+// Storing Data
+//var name ="ashley";
+
+//Functions
+//Functions are actions that we can use on our site
+//alert("Hey im a super annoying inbuilt js function");
+
+//Structuring a Functions
+//function name(parameter1,parameter2) {
+  //Instructions to carry out
+//};
+
+// jquery
+
+  // $("a").on("click", function() {
+  //   //Changes ccs properties
+  //   //$("body").css("background-color","blue");
+  // 
+  //   // Show/Hide our div
+  //   //$("#sweet-box").hide();
+  //   //$("#sweet-box").show();
+  //
+  //   // Fade in/out
+  //   //$("#sweet-box").fadeToggle();
+  //
+  //   //Add/Remove classes
+  //   $("#sweet-box").toggleClass("rainbow");
+  //
+  //
+  // });
+  //
+  // $("a.exit").on("click", function() {
+  //   $("#sweet-box").hide();
+  // });
